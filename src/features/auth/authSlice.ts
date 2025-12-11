bd6f782673e26fee5690555944ae5f9fe38d1ef4
NP-Thanh <22130259@st.hcmuaf.edu.vn>
2025-12-11 17:30:32 +0700

    xu ly api register co ban

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "./authTypes";
+
+interface AuthState {
+    user: User | null;
+}
+
+const initialState: AuthState = {
+    user: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<User>) => {
+            state.user = action.payload;
+        }
+    },
+});
+
+export const { setUser } = authSlice.actions;
+export default authSlice.reducer;
